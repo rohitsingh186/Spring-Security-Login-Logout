e29f80200c5fb369bfc56cce9a604763ee62927e
Rohit Singh <rohitsingh17219@gmail.com>
2016-11-21 15:54:53 +0530

    custom login working

--- src/main/java/com/barclays/controller/HelloController.java
+++ src/main/java/com/barclays/controller/HelloController.java
@@ -9,18 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
-	public ModelAndView welcomePage() {
-
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Hello World");
-		model.addObject("message", "This is welcome page!");
-		model.setViewName("hello");
-		return model;
-
-	}
-
-	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	@RequestMapping(value = {"/admin**"}, method = RequestMethod.GET)
 	public ModelAndView adminPage() {
 
 		ModelAndView model = new ModelAndView();
